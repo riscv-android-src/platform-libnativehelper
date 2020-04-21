85a203254a1203e8784e722eb77878b8c94dd642
Orion Hodson <oth@google.com>
2020-04-21 13:34:14 +0100

    Clean-up JniInvocation
    Removes awkward C++ inversion in JniInvocation introduced when
    changing to external C API for mainline.
    
    Bug: 151443957
    Test: m & boot
    Test: atest JniInvocation_test
    Change-Id: Id46ffc1ce5b550c1e938810fa24b0f9fad8275f4

---
+++ JniInvocation-priv.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LIBNATIVEHELPER_JNIINVOCATION_PRIV_H_
+#define LIBNATIVEHELPER_JNIINVOCATION_PRIV_H_
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+const char* JniInvocationGetLibraryWith(const char* library, char* buffer,
+                                        int (*is_debuggable)(),
+                                        int (*get_library_system_property)(char* buffer));
+
+__END_DECLS
+
+#endif  // LIBNATIVEHELPER_JNIINVOCATION_PRIV_H_
